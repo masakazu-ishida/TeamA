3ae79a88a42afeff61cbab975f0ece3eba63260c
morita-ma <morita-ma@AXPC-1555N.TTRDOM.local>
2025-06-13 11:29:22 +0900

    CategoriesDTO,DAO

---
+++ src/main/java/jp/co/brmy/dto/CategoriesDTO.java
@@ -0,0 +1,25 @@
+package jp.co.brmy.dto;
+
+public class CategoriesDTO {
+
+		private int category;
+		private String name;
+		
+		
+		public int getCategory() {
+			return category;
+		}
+		public void setCategory(int category) {
+			this.category = category;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		
+		
+		
+		
+}
