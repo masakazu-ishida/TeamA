8f1436654839f0d8da8d642c57a5dbc273e4da7e
kawami.n <kawami.n@AXPC-1480N.TTRDOM.local>
2025-06-17 11:58:15 +0900

    aaaaaaa

--- src/main/java/jp/co/brmy/dto/AdministratorsDTO.java
+++ src/main/java/jp/co/brmy/dto/AdministratorsDTO.java
@@ -3,24 +3,30 @@ package jp.co.brmy.dto;
 public class AdministratorsDTO {
 	private String adminId;
 	private String password;
-	private String name ;
-	
+	private String name;
+
 	public String getAdminId() {
 		return adminId;
 	}
+
 	public void setAdminId(String adminId) {
 		this.adminId = adminId;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 }
