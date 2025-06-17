7a7b5a641ab842eeff022301add39b4b78d711c2
kawami.n <kawami.n@AXPC-1480N.TTRDOM.local>
2025-06-17 14:21:26 +0900

    a

--- src/main/java/jp/co/brmy/dao/AdministratorsLoginDAO.java
+++ src/main/java/jp/co/brmy/dao/AdministratorsLoginDAO.java
@@ -8,29 +8,28 @@ import java.sql.SQLException;
 import jp.co.brmy.dto.AdministratorsDTO;
 
 public class AdministratorsLoginDAO extends BaseDAO {
-	
+
 	public AdministratorsLoginDAO(Connection conn) {
 		super(conn);
 		// TODO 自動生成されたコンストラクター・スタブ
 	}
 
-	public AdministratorsDTO findById(String adminId,String pass) throws SQLException{
-		String sql ="SELECT admin_id,password, name from administrators where admin_id = ? and password = ?";
+	public AdministratorsDTO findById(String adminId) throws SQLException {
+		String sql = "SELECT admin_id,password, name from administrators where admin_id = ? and password = ?";
 		AdministratorsDTO adminDto = null;
-		
-		try (PreparedStatement ps = conn.prepareStatement(sql)){
+
+		try (PreparedStatement ps = conn.prepareStatement(sql)) {
 			ps.setString(1, adminId);
-			ps.setString(2, pass);
-			
+
 			System.out.println(ps);
 			ResultSet rs = ps.executeQuery();
-			
-			if(rs.next()) {
+
+			if (rs.next()) {
 				adminDto = new AdministratorsDTO();
 				adminDto.setAdminId(rs.getString("admin_id"));
 				adminDto.setPassword(rs.getString("password"));
-			}	
-		}return adminDto;
+			}
+		}
+		return adminDto;
 	}
 }
-
