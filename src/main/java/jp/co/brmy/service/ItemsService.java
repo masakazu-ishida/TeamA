3b24ab4d9d3cabc97fef8f8a14fd0e80775cccce
morita-ma <morita-ma@AXPC-1555N.TTRDOM.local>
2025-06-19 11:44:41 +0900

    Merge branch 'master' of https://github.com/masakazu-ishida/TeamA.git

--- src/main/java/jp/co/brmy/service/ItemsService.java
+++ src/main/java/jp/co/brmy/service/ItemsService.java
@@ -16,7 +16,42 @@ import jp.co.brmy.util.ConnectionUtil;
 
 public class ItemsService {
 
-	public List<ItemsDTO> findNameSearch(String name, int id) throws SQLException, ServletException {
+	//	public List<ItemsDTO> findNameSearch(String name, int id)
+	//			throws SQLException, ServletException {
+	//		String lookupstring = "java:comp/env/jdbc/ecsite";
+	//		try (Connection conn = ConnectionUtil.getConnection(lookupstring)) {
+	//			//itemsDAOのfindnamesearchを呼び出し、戻り値をそのままリターン
+	//
+	//			ItemsDAO itemsDao = new ItemsDAO(conn);
+	//
+	//			List<ItemsDTO> itemsDto = new ArrayList<>();
+	//
+	//			//キーワードが非空かつ、カテゴリ名が全部の場合
+	//			if ((name != null && name != "") && id == 0) {
+	//				itemsDto = itemsDao.findNameSearch(name);
+	//			}
+	//
+	//			//キーワードが非空かつ、カテゴリ名が何か指定されている場合
+	//			else if ((name != null && name != "") && id != 0) {
+	//				itemsDto = itemsDao.findNameSearch(name, id);
+	//			}
+	//
+	//			//キーワードが空白かつ、カテゴリ名が何か指定されている場合
+	//			else if ((name == "" || name == null) && id != 0) {
+	//				itemsDto = itemsDao.findNameSearch(id);
+	//			}
+	//
+	//			//キーワードが空白かつ、カテゴリ名が全部の場合
+	//			else if ((name == "" || name == null) && id == 0) {
+	//				itemsDto = itemsDao.findAll();
+	//			}
+	//
+	//			return itemsDto;
+
+	//}}
+
+	public List<ItemsDTO> findNameSearchByLimit(String name, int id, int pageNumber)
+			throws SQLException, ServletException {
 		String lookupstring = "java:comp/env/jdbc/ecsite";
 		try (Connection conn = ConnectionUtil.getConnection(lookupstring)) {
 			//itemsDAOのfindnamesearchを呼び出し、戻り値をそのままリターン
@@ -27,17 +62,17 @@ public class ItemsService {
 
 			//キーワードが非空かつ、カテゴリ名が全部の場合
 			if ((name != null && name != "") && id == 0) {
-				itemsDto = itemsDao.findNameSearch(name);
+				itemsDto = itemsDao.findNameSearchByLimit(name, pageNumber);
 			}
 
 			//キーワードが非空かつ、カテゴリ名が何か指定されている場合
 			else if ((name != null && name != "") && id != 0) {
-				itemsDto = itemsDao.findNameSearch(name, id);
+				itemsDto = itemsDao.findNameSearchByLimit(name, id, pageNumber);
 			}
 
 			//キーワードが空白かつ、カテゴリ名が何か指定されている場合
 			else if ((name == "" || name == null) && id != 0) {
-				itemsDto = itemsDao.findNameSearch(id);
+				itemsDto = itemsDao.findNameSearchByLimit(id, pageNumber);
 			}
 
 			//キーワードが空白かつ、カテゴリ名が全部の場合
