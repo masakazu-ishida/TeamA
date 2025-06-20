8b9902ea3edc0b32ea040e4e07fc29823661e551
kawami.n <kawami.n@AXPC-1480N.TTRDOM.local>
2025-06-20 10:46:09 +0900

    a

---
+++ src/main/java/jp/co/brmy/controller/DeletePurchaseConfirmController.java
@@ -0,0 +1,67 @@
+package jp.co.brmy.controller;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import jp.co.brmy.dto.PurchasesDTO;
+import jp.co.brmy.service.DeletePurchaseConfirmService;
+
+/**
+ * Servlet implementation class DeletePurchaseConfirmController
+ */
+@WebServlet(name = "DeletePurchaseConfirmController", urlPatterns = { "/DeletePurchaseConfirmController" })
+public class DeletePurchaseConfirmController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public DeletePurchaseConfirmController() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String purchaseIdParam = request.getParameter("purchasId");
+		String path = "/WEB-INF/jsp/deletePurchaseConfirm.jsp";
+
+		int purchaseId = Integer.parseInt(purchaseIdParam);
+
+		DeletePurchaseConfirmService dpcc = new DeletePurchaseConfirmService();
+		PurchasesDTO dto = new PurchasesDTO();
+
+		try {
+			dto = dpcc.findById(purchaseId);
+
+		} catch (SQLException | ServletException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		request.setAttribute("dto", dto);
+		RequestDispatcher rd = request.getRequestDispatcher(path);
+		rd.forward(request, response);
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/java/jp/co/brmy/controller/PurchaseSearchController.java
@@ -0,0 +1,72 @@
+package jp.co.brmy.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import jp.co.brmy.dto.ItemsDTO;
+import jp.co.brmy.dto.PurchaseDetailsDTO;
+import jp.co.brmy.dto.PurchasesDTO;
+import jp.co.brmy.service.PurchaseSearchService;
+
+/**
+ * Servlet implementation class PurchaseSerchController
+ */
+@WebServlet(name = "PurchaseSearchController", urlPatterns = { "/PurchaseSearchController" })
+public class PurchaseSearchController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public PurchaseSearchController() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+
+		String path1 = "/WEB-INF/jsp/adminmain.jsp";
+		String path2 = "/WEB-INF/jsp/purchaseSearch.jsp";
+		String name = request.getParameter("userId");
+
+		PurchaseSearchService service = new PurchaseSearchService();
+		List<PurchasesDTO> list = new ArrayList<>();
+		try {
+			list = service.findByName(name);
+
+			if (list.size() == 0) {
+				RequestDispatcher rd = request.getRequestDispatcher(path1);
+				rd.forward(request, response);
+			} else {
+				request.setAttribute("purchas", list);
+				RequestDispatcher rd = request.getRequestDispatcher(path2);
+				rd.forward(request, response);
+			}
+		} catch (Exception e) {
+		}
+	}
+}
