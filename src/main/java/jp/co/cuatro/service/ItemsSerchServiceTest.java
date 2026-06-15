2a469fa9d2c748dca4796a1903c21403e303684a
Sm20260401 <morita-s@ita-school.jp>
2026-06-15 13:16:47 +0900

    Merge branch 'master' of https://github.com/masakazu-ishida/TeamA.git

---
+++ src/main/java/jp/co/cuatro/service/ItemsSerchServiceTest.java
@@ -0,0 +1,63 @@
+package jp.co.cuatro.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import jakarta.servlet.ServletException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import jp.co.cuatro.dto.ItemDTO;
+import jp.co.cuatro.util.TestBase;
+
+class ItemsSerchServiceTest extends TestBase {
+	@BeforeEach
+	void setUp() throws Exception {
+		String sqlFilePath = "/init_data.sql";
+		super.initSQLFiles(sqlFilePath);
+	}
+
+	@Test
+	void testFindAll() {
+
+		ItemsSerchService item = new ItemsSerchService();
+		try {
+			List<ItemDTO> itemsList = item.execute(1, "");
+			assertNotNull(itemsList);
+			assertEquals(11, itemsList.size());
+
+			itemsList = item.execute(1, "帽");
+			assertNotNull(itemsList);
+			assertEquals(5, itemsList.size());
+
+			itemsList = item.execute(2, "");
+			assertNotNull(itemsList);
+			assertEquals(9, itemsList.size());
+
+			itemsList = item.execute(2, "E");
+			assertNotNull(itemsList);
+			assertEquals(1, itemsList.size());
+
+			itemsList = item.execute(3, "");
+			assertNotNull(itemsList);
+			assertEquals(20, itemsList.size());
+
+			itemsList = item.execute(3, "A");
+			assertNotNull(itemsList);
+			assertEquals(3, itemsList.size());
+
+			itemsList = item.execute(3, "111");
+			assertNotNull(itemsList);
+			assertEquals(0, itemsList.size());
+
+		} catch (ServletException e) {
+
+			e.printStackTrace();
+			fail(e);
+		}
+
+	}
+
+}
