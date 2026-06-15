3ac9ea5d20fa62697eed53e55123337c55dae715
nakase-h <nakase-h@ita-school.jp>
2026-06-15 10:35:50 +0900

    UsersDTO作成

---
+++ src/main/java/jp/co/cuatro/dto/UsersDTO.java
@@ -0,0 +1,41 @@
+package jp.co.cuatro.dto;
+
+public class UsersDTO {
+	private String userId; // 会員ID
+	private String password; // パスワード
+	private String name; // 名前
+	private String address; // 住所
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
