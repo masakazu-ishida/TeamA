3cbe374188a607152902b17f6510ae370d2faf8e
Sm20260401 <morita-s@ita-school.jp>
2026-06-15 13:57:02 +0900

    追加

---
+++ src/main/java/jp/co/cuatro/servlet/ItemsSerchController.java
@@ -0,0 +1,62 @@
+package jp.co.cuatro.servlet;
+
+import java.io.IOException;
+import java.util.List;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import jp.co.cuatro.dto.ItemDTO;
+import jp.co.cuatro.service.ItemsSerchService;
+
+/**
+ * Servlet implementation class ItemsSerchController
+ */
+@WebServlet("/ItemsSerch")
+public class ItemsSerchController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public ItemsSerchController() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+
+		int categoryId = Integer.parseInt(request.getParameter("categoryId"));
+		String name = request.getParameter("name");
+
+		ItemsSerchService itemSerch = new ItemsSerchService();
+
+		List<ItemDTO> itemsList = itemSerch.execute(categoryId, name);
+
+		request.setAttribute("itemsList", itemsList);
+		request.setAttribute("itemsList", itemsList);
+
+		String path = "/itemsResult.jsp";
+		RequestDispatcher rd = request.getRequestDispatcher(path);
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/itemsResult.jsp
@@ -0,0 +1,31 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="jakarta.tags.core" %> 
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<h3>キーワード "<%= request.getParameter("keyword") %>" カテゴリ "<%= request.getParameter("cnumber") %>" の検索結果</h3>
+		<br />
+		<table border="1">
+			<tr>
+				<th>商品名</th>
+				<th>商品の色</th>
+				<th>メーカー名</th>
+				<th>価格</th>
+			</tr>
+			<c:forEach items="${itemsList}" var="item">
+       			<tr>
+            		<td><a href='itemDetail.html?itemId=123'><c:out value="${item.getItemName()}" /></a></td>
+            		<td><a href='itemDetail.html?itemId=123'><c:out value="${item.getColor()}" /></a></td>
+            		<td><a href='itemDetail.html?itemId=123'><c:out value="${item.getManufacturer()}" /></a></td>
+            		<td><a href='itemDetail.html?itemId=123'><c:out value="${item.getPrice()}" /></a></td>    
+        		</tr>
+    		</c:forEach>
+		</table>
+<br><a href='main.jsp'>商品検索</a>へ
+</body>
+</html>
