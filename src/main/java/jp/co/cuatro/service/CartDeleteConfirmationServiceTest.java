2b65239b2fd647a655d8bdec3f251d57574d70ba
tn-create3 <nakajima-t@AXPC-7142N.TTRDOM.local>
2026-06-19 09:35:48 +0900

    カート削除表示処理のサービステストを修正しました。

--- src/main/java/jp/co/cuatro/service/CartDeleteConfirmationServiceTest.java
+++ src/main/java/jp/co/cuatro/service/CartDeleteConfirmationServiceTest.java
@@ -6,17 +6,31 @@ import java.time.LocalDate;
 
 import jakarta.servlet.ServletException;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import jp.co.cuatro.dto.CartDTO;
+import jp.co.cuatro.util.TestBase;
 
-class CartDeleteConfirmationServiceTest {
+class CartDeleteConfirmationServiceTest extends TestBase {
+
+	/**
+	 * 
+	 * 以下の初期化メソッドは何も考えずまるごとコピー
+	 * テストメソッド実行の度に毎回init_data.sqlが実行され、
+	 * DBの中身が初期化される
+	 * */
+	@BeforeEach
+	void setUp() throws Exception {
+		String sqlFilePath = "/init_data.sql";
+		super.initSQLFiles(sqlFilePath);
+	}
 
 	/**
 	 * カート内に存在する商品の削除確認表示のテスト
 	 */
 	@Test
-	void testCartDeleteConfirm1() {
+	void testCartDeleteConfirm() {
 
 		CartDeleteConfirmationService svc = new CartDeleteConfirmationService();
 		try {
@@ -31,7 +45,7 @@ class CartDeleteConfirmationServiceTest {
 			assertNotNull(resultCart);
 			assertEquals("user1", resultCart.getUserId());
 			assertEquals(1, resultCart.getItemId());
-			assertEquals(6, resultCart.getAmount());
+			assertEquals(1, resultCart.getAmount());
 			assertEquals(LocalDate.of(2026, 6, 15), resultCart.getBookedDate());
 
 		} catch (ServletException e) {
